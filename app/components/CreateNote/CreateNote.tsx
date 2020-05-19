93003c6dc88561b8203297ad5f1a118a1c5fc02d
Hkataya <Hkataya@users.noreply.github.com>
2020-05-19 18:20:54 +0300

    Add CreateNote UI

--- app/components/CreateNote/CreateNote.tsx
+++ app/components/CreateNote/CreateNote.tsx
@@ -1,13 +1,41 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 
 import React, { useState } from 'react';
+import styled from 'styled-components';
+import TextareaAutosize from 'react-autosize-textarea';
 import { NoteActionCreatorType } from '../../actions/notes';
+
+const Wrapper = styled.div.attrs({
+  className: 'rounded-lg shadow-lg bg-white my-3'
+})``;
+
+const Header = styled.div.attrs({
+  className: 'flex justify-between  px-3 py-2'
+})``;
+
+const Body = styled.div.attrs({
+  className: 'px-3 py-2 text-gray-600'
+})``;
+
+const ButtonWrapper = styled.div.attrs({
+  className: 'px-3 py-2'
+})``;
+
+const TitleInput = styled.input.attrs({
+  className:
+    'appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-700'
+})``;
+
+const StyledTextareaAutosize = styled(TextareaAutosize).attrs({
+  className:
+    'appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-700'
+})``;
 
 type Props = NoteActionCreatorType & {
   videoId: string;
 };
 
-const AddVideoForm = (props: Props) => {
+const CreateNote = (props: Props) => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const { addNote, videoId } = props;
@@ -23,56 +51,31 @@ const AddVideoForm = (props: Props) => {
   };
 
   return (
-    <form className="w-full max-w-sm" onSubmit={handleSubmit}>
-      <div className="md:flex md:items-center mb-6">
-        <div className="md:w-1/3">
-          <label
-            className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-            htmlFor="inline-full-name"
-          >
-            Title
-          </label>
-        </div>
-        <div className="md:w-2/3">
-          <input
-            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-            type="text"
+    <form className="w-full" onSubmit={handleSubmit}>
+      <Wrapper>
+        <Header>
+          <TitleInput
             placeholder="title"
             onChange={e => setTitle(e.target.value)}
           />
-        </div>
-      </div>
-      <div className="md:flex md:items-center mb-6">
-        <div className="md:w-1/3">
-          <label
-            className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-            htmlFor="inline-username"
-          >
-            Description
-          </label>
-        </div>
-        <div className="md:w-2/3">
-          <input
-            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-            type="text"
-            placeholder="url"
+        </Header>
+        <Body>
+          <StyledTextareaAutosize
+            placeholder="descrtipion"
             onChange={e => setDescription(e.target.value)}
           />
-        </div>
-      </div>
-      <div className="md:flex md:items-center">
-        <div className="md:w-1/3" />
-        <div className="md:w-2/3">
+        </Body>
+        <ButtonWrapper>
           <button
-            className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
             type="submit"
+            className="w-full focus:outline-none px-3 py-2 bg-purple-900 text-white font-bold uppercase rounded"
           >
-            Done
+            +
           </button>
-        </div>
-      </div>
+        </ButtonWrapper>
+      </Wrapper>
     </form>
   );
 };
 
-export default AddVideoForm;
+export default CreateNote;
