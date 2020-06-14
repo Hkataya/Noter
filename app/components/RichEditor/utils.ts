ef1a453d2f44aff4cd3d57d4b0c6e88e466a605c
Sami Shames El Deen <47642506+Sami-Sh99@users.noreply.github.com>
2020-06-14 20:08:43 +0300

    Create Draft.js Utils

---
+++ app/components/RichEditor/utils.ts
@@ -0,0 +1,24 @@
+import {
+  EditorState,
+  convertFromRaw,
+  convertToRaw,
+  ContentState
+} from 'draft-js';
+
+export const InitializeForDraftEditor = (text: string): string => {
+  return JSON.stringify(convertToRaw(ContentState.createFromText(text)));
+};
+
+export const convertToEditorState = (text: string): EditorState => {
+  return EditorState.createWithContent(convertFromRaw(JSON.parse(text)));
+};
+
+export const extractPlainText = (text: string): string => {
+  return convertFromRaw(JSON.parse(text)).getPlainText();
+};
+
+export const convertFromEditorStateToString = (
+  editorState: EditorState
+): string => {
+  return JSON.stringify(convertToRaw(editorState.getCurrentContent()));
+};
