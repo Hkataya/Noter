c1b1fe63ca2510d1dcf74cb5165938c50e96b603
Hkataya <Hkataya@users.noreply.github.com>
2020-06-22 11:09:27 +0300

    Update video count on section deletion

--- app/db/SectionRepository.ts
+++ app/db/SectionRepository.ts
@@ -8,6 +8,18 @@ class SectionRepository extends Repository<SectionType> {
     super(db, relDB, 'section');
   }
 
+  deleteEntity(entityId: string) {
+    return this.relDB.rel.find('section', entityId).then(data => {
+      return super.deleteEntity(entityId).then(response => {
+        return this.getCourseAndUpdateVideoCount(data.sections[0].course).then(
+          () => {
+            return response;
+          }
+        );
+      });
+    });
+  }
+
   getSectionsByCourseId = (courseId: CourseType['id']) => {
     return this.db
       .createIndex({ index: { fields: ['data.course', '_id'] } })
@@ -16,6 +28,15 @@ class SectionRepository extends Repository<SectionType> {
           .findHasMany('section', 'course', courseId)
           .then(data => sortArrayByDateCreated(data.sections));
       });
+  };
+
+  getCourseAndUpdateVideoCount = (courseId: string) => {
+    return this.relDB.rel.find('course', courseId).then(data => {
+      const course = data.courses[0];
+      const videoCount = data.videos.length;
+      course.videoCount = videoCount;
+      return this.relDB.rel.save('course', course);
+    });
   };
 }
 
